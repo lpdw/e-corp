da95905e5e9a817daa482c087a8671b87b5e029b
kerfendal yann <kerfendalyann@gmail.com>
2017-04-04 18:40:11 +0200

    modif totale projet, issue cours jomaora

--- app.js
+++ app.js
@@ -4,15 +4,26 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var methodOverride = require('method-override');
+var cors = require('cors')
 
+var APIError = require('./lib/apiError');
 var index = require('./routes/index');
 var users = require('./routes/users');
+var signup = require('./routes/signup');
+var login = require('./routes/login');
+var logout = require('./routes/logout');
+
+var passport = require('passport');
+var authentication = require('./services/authentication');
 
 var app = express();
 
+app.use(cors());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +33,63 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var sess = {
+    secret: 'social-music-api',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true
+};
+app.use(session(sess));
+app.use(methodOverride('_method'));
+
+app.use((req, res, next) => {
+    if (!req.accepts('text/html') && !req.accepts('application/json')) {
+        return next(new APIError(406, 'Not valid type for asked resource'));
+    }
+    next();
+});
+
+passport.serializeUser(function(user, done) {
+   done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+   done(null, obj);
+});
+
+passport.use(authentication.songApiLocalStrategy());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const verifyAuth = (req, res, next) => {
+   res.locals.userLogged = false;
+   if (req.originalUrl === '/signup' || req.originalUrl === '/login') {
+       return next();
+   }
+   if (req.get('authorization') === 'lpdw-2016') {
+       res.locals.userLogged = true;
+       return next();
+   }
+   if (req.isAuthenticated()) {
+       res.locals.userLogged = true;
+       return next();
+   }
+   if (req.accepts('text/html')) {
+        return res.redirect('/login');
+   }
+   if (req.accepts('application/json')) {
+        res.set('Location', '/login');
+        return res.status(401).send({err: 'User should be logged'});
+   }
+};
+app.all('*', verifyAuth);
+
 app.use('/', index);
 app.use('/users', users);
+app.use('/signup', signup);
+app.use('/login', login);
+app.use('/logout', logout);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,7 +106,10 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  if (req.accepts('text/html')) {
+    return res.render('error');
+  }
+  res.send(res.locals.message);
 });
 
 module.exports = app;
---
+++ routes/login.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const APIError = require('../lib/apiError');
+const passport = require('passport');
+
+router.get('/', (req, res) => {
+    if (req.accepts('text/html')) {
+        return res.render('login');
+    }
+    next(new APIError(406, 'Not valid type for asked ressource'));
+});
+
+router.post('/',
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/login'
+    })
+);
+
+module.exports = router;
