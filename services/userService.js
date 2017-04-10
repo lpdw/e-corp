e9833b70f206cb06f127450eb806fb1d985e5050
smargaux <smits.margaux@gmail.com>
2017-04-10 18:04:24 +0200

    Fix on sign up

--- services/userService.js
+++ services/userService.js
@@ -19,12 +19,15 @@ exports.createUser = user => {
            var accountNB=uid.sync(12);
            var token = uid.sync(8);
            // Une fois l'utilisateur enregistré, on lui créé un compte bancaire
-           const account = db.Accounts.build({credit:150,account_nb:accountNB,token:token});
+           db.Accounts.create({credit:150,account_nb:accountNB,token:token}).then(
+             account => {
+               // On associe le compte à l'utilisateur crée
+               model.setAccount(account);
+              return model.save();
+             }
+           );
 
-           // On associe le compte à l'utilisateur crée
-           model.setAccount(account);
-           account.save();
-            return model.save();
+
         })
     ;
 };
